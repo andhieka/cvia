71d8517a022893a90d98bdaee5ee86cce1e13bb1
John Kevin Tjahjadi <johnkevin@186-130.priv23.nus.edu.sg>
2015-10-10 22:27:44 +0800

    generated getter and setter for CVParseResult

--- src/main/java/cvia/model/CVParseResult.java
+++ src/main/java/cvia/model/CVParseResult.java
@@ -8,6 +8,7 @@ import java.util.List;
  * Created by Michael Limantara on 9/23/2015.
  */
 public class CVParseResult {
+
     private PersonalInfo personalInfo;
     private List<EducationInfo> educationInfoList;
     private List<WorkExperience> workExperienceList;
@@ -15,4 +16,62 @@ public class CVParseResult {
     private List<Skill> skills;
     private List<WorkExperience> workExperiences;
     private String rawContents;
+
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    public List<EducationInfo> getEducationInfoList() {
+        return educationInfoList;
+    }
+
+    public void setEducationInfoList(List<EducationInfo> educationInfoList) {
+        this.educationInfoList = educationInfoList;
+    }
+
+    public List<WorkExperience> getWorkExperienceList() {
+        return workExperienceList;
+    }
+
+    public void setWorkExperienceList(List<WorkExperience> workExperienceList) {
+        this.workExperienceList = workExperienceList;
+    }
+
+    public List<Language> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<Language> languages) {
+        this.languages = languages;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public List<WorkExperience> getWorkExperiences() {
+        return workExperiences;
+    }
+
+    public void setWorkExperiences(List<WorkExperience> workExperiences) {
+        this.workExperiences = workExperiences;
+    }
+
+    public String getRawContents() {
+        return rawContents;
+    }
+
+    public void setRawContents(String rawContents) {
+        this.rawContents = rawContents;
+    }
+
+
 }
