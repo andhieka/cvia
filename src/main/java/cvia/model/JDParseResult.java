4dfaf1a3751ebd251c981ee47767d848f73e7dfc
Andhieka Putra <andhieka.putra@gmail.com>
2015-10-10 23:10:53 +0800

    Add JDParseResult

--- src/main/java/cvia/model/JDParseResult.java
+++ src/main/java/cvia/model/JDParseResult.java
@@ -6,7 +6,52 @@ import java.util.List;
  * Created by Michael Limantara on 9/23/2015.
  */
 public class JDParseResult {
-    List<Skill> requiredSkills;
-    EducationInfo minimumEducation;
+    // Must be used by the Analyzer to match agains CVParseResult
+    private List<Skill> requiredSkills;
+    private EducationInfo minimumEducation;
+    private Float minimumYearsOfWorkExperience;
+    private List<Language> requiredLanguages;
 
+    // For manual verification by humans
+    private List<String> responsibilities;
+
+    public List<Skill> getRequiredSkills() {
+        return requiredSkills;
+    }
+
+    public void setRequiredSkills(List<Skill> requiredSkills) {
+        this.requiredSkills = requiredSkills;
+    }
+
+    public EducationInfo getMinimumEducation() {
+        return minimumEducation;
+    }
+
+    public void setMinimumEducation(EducationInfo minimumEducation) {
+        this.minimumEducation = minimumEducation;
+    }
+
+    public Float getMinimumYearsOfWorkExperience() {
+        return minimumYearsOfWorkExperience;
+    }
+
+    public void setMinimumYearsOfWorkExperience(Float minimumYearsOfWorkExperience) {
+        this.minimumYearsOfWorkExperience = minimumYearsOfWorkExperience;
+    }
+
+    public List<Language> getRequiredLanguages() {
+        return requiredLanguages;
+    }
+
+    public void setRequiredLanguages(List<Language> requiredLanguages) {
+        this.requiredLanguages = requiredLanguages;
+    }
+
+    public List<String> getResponsibilities() {
+        return responsibilities;
+    }
+
+    public void setResponsibilities(List<String> responsibilities) {
+        this.responsibilities = responsibilities;
+    }
 }
