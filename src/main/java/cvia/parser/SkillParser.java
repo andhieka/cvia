fe61571c9c4d7f414bf32471022952c776cb5052
Andhieka Putra <andhieka.putra@gmail.com>
2015-10-13 04:02:22 +0800

    Add JSON reader to Gradle dependency

---
+++ src/main/java/cvia/parser/SkillParser.java
@@ -0,0 +1,14 @@
+package cvia.parser;
+
+import java.util.TreeSet;
+
+/**
+ * Created by andhieka on 13/10/15.
+ */
+public class SkillParser {
+    TreeSet<String> keywords;
+
+    public SkillParser() {
+
+    }
+}
