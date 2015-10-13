7909a456b030fabe63f0e2e122a7549729ca4a2e
John Kevin Tjahjadi <jk_john_kevin@hotmail.com>
2015-10-13 11:33:41 +0800

    add getter-setter and attributes for Report

--- src/main/java/cvia/analyzer/Report.java
+++ src/main/java/cvia/analyzer/Report.java
@@ -8,14 +8,45 @@ import java.util.LinkedList;
 /**
  * Created by Michael Limantara on 9/23/2015.
  */
-public class Report {
+public class Report implements Comparable<Report>{
     private PersonalInfo biodata;
+
+    public List<Skill> getMatchedSkills() {
+        return matchedSkills;
+    }
+
     private List<Skill> matchedSkills;
+
+    public PersonalInfo getBiodata() {
+        return biodata;
+    }
+
+    public List<Skill> getUnmatchedSkills() {
+        return unmatchedSkills;
+    }
+
+    public List<Skill> getExtraSkills() {
+        return extraSkills;
+    }
+
+    public List<Language> getMatchedLanguages() {
+        return matchedLanguages;
+    }
+
+    public List<Language> getUnmatchedLanguages() {
+        return unmatchedLanguages;
+    }
+
+    public List<Language> getExtraLanguages() {
+        return extraLanguages;
+    }
+
     private List<Skill> unmatchedSkills;
     private List<Skill> extraSkills;
     private List<Language> matchedLanguages;
     private List<Language> unmatchedLanguages;
     private List<Language> extraLanguages;
+    private int score;
 
     public Report(PersonalInfo biodata) {
         this.biodata = biodata;
@@ -51,6 +82,24 @@ public class Report {
 
     public void addUnmatchedLanguage(Language unmatchedLanguage) {
         this.unmatchedLanguages.add(unmatchedLanguage);
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int compareTo(Report other) {
+        if (this.getScore() < other.getScore()) {
+            return -1;
+        } else if (this.getScore() > other.getScore()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
 
