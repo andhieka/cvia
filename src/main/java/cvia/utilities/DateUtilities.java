3812ba6ed6bda22d0af228a3f4c20944e5a8963c
Michael <mike.bdg@gmail.com>
2015-11-09 04:55:35 +0800

    Implement page numbering for pdf

--- src/main/java/cvia/utilities/DateUtilities.java
+++ src/main/java/cvia/utilities/DateUtilities.java
@@ -8,10 +8,16 @@ import java.time.format.DateTimeFormatter;
  */
 public class DateUtilities {
     public static LocalDate dateFromString(String dateString, DateTimeFormatter dateTimeFormatter) {
+        if (dateString.isEmpty()) {
+            return null;
+        }
         return LocalDate.parse(dateString, dateTimeFormatter);
     }
 
     public static String getDateString(LocalDate date, DateTimeFormatter dateTimeFormatter) {
+        if (date == null) {
+            return "";
+        }
         return date.format(dateTimeFormatter);
     }
 }
