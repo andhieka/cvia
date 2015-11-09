3e75f823115d97a87f4a5dda2b783a3b189bf409
Andhieka Putra <andhieka.putra@gmail.com>
2015-11-10 03:37:48 +0800

    Add university bank

---
+++ src/main/java/cvia/parser/UniversityBank.java
@@ -0,0 +1,68 @@
+package cvia.parser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by andhieka on 10/11/15.
+ */
+public class UniversityBank {
+    public static final String FILENAME_UNIVERSITIES_LIST = "world_universities_and_domains.json";
+    private ArrayList<String> universityNames = new ArrayList<String>();
+    private ArrayList<String> universityAcronyms = new ArrayList<String>();
+    private HashMap<String, String> acronymToNameDict = new HashMap<String, String>();
+    private static UniversityBank instance;
+
+    public static UniversityBank getInstance() {
+        if (instance == null) {
+            instance = new UniversityBank();
+        }
+        return instance;
+    }
+
+    private UniversityBank() {
+        loadData();
+    }
+
+    public List<String> getUniversityNames() {
+        return universityNames;
+    }
+
+    public List<String> getUniversityAcronyms() {
+        return universityAcronyms;
+    }
+
+    public String fullNameOfAcronym(String acronym) {
+        return acronymToNameDict.get(acronym.toLowerCase());
+    }
+
+    // Private methods
+    private void loadData() {
+        try {
+            InputStream inputStream = new FileInputStream(FILENAME_UNIVERSITIES_LIST);
+            JSONTokener tokener = new JSONTokener(inputStream);
+            JSONArray universities = new JSONArray(tokener);
+            for (int i = 0; i < universities.length(); i++) {
+                JSONObject university = universities.getJSONObject(i);
+                String acronym = university.getString("domain").split("\\.")[0];
+                String name = university.getString("name");
+                universityNames.add(name);
+                universityAcronyms.add(acronym.toLowerCase());
+                acronymToNameDict.put(acronym.toLowerCase(), name);
+            }
+        } catch (FileNotFoundException e) {
+            Logger logger = Logger.getGlobal();
+            logger.log(Level.SEVERE, String.format("Cannot find languages list (%s)", FILENAME_UNIVERSITIES_LIST), e);
+        }
+    }
+}
---
+++ src/test/java/cvia/parser/UniversityBankTest.java
@@ -0,0 +1,32 @@
+package cvia.parser;
+
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andhieka on 10/11/15.
+ */
+public class UniversityBankTest {
+    UniversityBank universityBank = UniversityBank.getInstance();
+
+    @Test
+    public void testGetNames() throws Exception {
+        List<String> universityNames = universityBank.getUniversityNames();
+        assertEquals(8271, universityNames.size());
+    }
+
+    @Test
+    public void testGetAcronyms() throws Exception {
+        List<String> universityAcronyms = universityBank.getUniversityAcronyms();
+        assertEquals(8271, universityAcronyms.size());
+    }
+
+    @Test
+    public void testAcronymToName() throws Exception {
+        assertEquals("National University of Singapore", universityBank.fullNameOfAcronym("NUS"));
+        assertEquals("National University of Singapore", universityBank.fullNameOfAcronym("nus"));
+        assertEquals("California Institute of Technology", universityBank.fullNameOfAcronym("CalTech"));g
+    }
+}
