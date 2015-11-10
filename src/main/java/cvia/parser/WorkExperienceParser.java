cc34a13db257ebc4f70b8650237439fc426e4c1d
Michael <mike.bdg@gmail.com>
2015-11-11 04:13:22 +0800

    Implement function to find daterange, position, company in work experience parser

--- src/main/java/cvia/parser/WorkExperienceParser.java
+++ src/main/java/cvia/parser/WorkExperienceParser.java
@@ -2,18 +2,28 @@ package cvia.parser;
 
 import cvia.model.CV;
 import cvia.reader_writer.TextChunk;
-import cvia.reader_writer.TextLine;
-import cvia.utilities.TextChunkUtilities;
+import org.json.JSONArray;
+import org.json.JSONTokener;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by andhieka on 9/11/15.
  */
 public class WorkExperienceParser implements MiniParser {
+    private static final String POSITION_KEYWORDS_FILENAME = "business_positions.json";
+    private static final String COMPANY_KEYWORDS_FILENAME = "company.json";
     private CV cv;
     private ArrayList<TextChunk> textChunks = new ArrayList<TextChunk>();
+
+    private DateRangeParser dateRangeParser = new DateRangeParser();
+    private ArrayList<String> positionKeywords = new ArrayList<String>();
+    private ArrayList<String> companyKeywords = new ArrayList<String>();
 
     @Override
     public void setCV(CV cv) {
@@ -28,16 +38,75 @@ public class WorkExperienceParser implements MiniParser {
     @Override
     public void parseAndSave() {
         assert(cv != null);
-
-        List<TextLine> textLines = TextChunkUtilities.combineLines(textChunks);
-        for (TextLine textLine:textLines) {
-            System.out.println(textLine.getText());
-        }
     }
 
     @Override
     public void reset() {
         cv = null;
         textChunks.clear();
+    }
+
+    private DateRange findDateRange(String line) {
+        DateRange dateRange = dateRangeParser.parse(line);
+        return dateRange;
+    }
+
+    private String findCompany(String line) {
+        loadCompanyKeywords();
+        String company = null;
+        Integer lengthOfMatch = 0;
+
+        for (String companyKeyword: companyKeywords) {
+            if (line.contains(company) && lengthOfMatch < companyKeyword.length()) {
+                company = companyKeyword;
+                lengthOfMatch = companyKeyword.length();
+            }
+        }
+
+        return company;
+    }
+
+    private String findPosition(String line) {
+        loadPositionKeywords();
+        String position = null;
+        Integer lengthOfMatch = 0;
+
+        for (String positionKeyword: positionKeywords) {
+            if (line.contains(positionKeyword) && lengthOfMatch < positionKeyword.length()) {
+                position = positionKeyword;
+                lengthOfMatch = positionKeyword.length();
+            }
+        }
+        return position;
+    }
+
+    private void loadPositionKeywords() {
+        try {
+            InputStream inputStream = new FileInputStream(POSITION_KEYWORDS_FILENAME);
+            JSONTokener tokener = new JSONTokener(inputStream);
+            JSONArray array = new JSONArray(tokener);
+            for (int i = 0; i < array.length(); i++) {
+                String keyword = array.getString(i);
+                positionKeywords.add(keyword);
+            }
+        } catch (FileNotFoundException e) {
+            Logger logger = Logger.getGlobal();
+            logger.log(Level.SEVERE, String.format("Cannot find keyword list (%s)", POSITION_KEYWORDS_FILENAME), e);
+        }
+    }
+
+    private void loadCompanyKeywords() {
+        try {
+            InputStream inputStream = new FileInputStream(COMPANY_KEYWORDS_FILENAME);
+            JSONTokener tokener = new JSONTokener(inputStream);
+            JSONArray array = new JSONArray(tokener);
+            for (int i = 0; i < array.length(); i++) {
+                String keyword = array.getString(i);
+                companyKeywords.add(keyword);
+            }
+        } catch (FileNotFoundException e) {
+            Logger logger = Logger.getGlobal();
+            logger.log(Level.SEVERE, String.format("Cannot find keyword list (%s)", COMPANY_KEYWORDS_FILENAME), e);
+        }
     }
 }
